4cae881b2083bc08a6ebec07660dcf0b8c9985af
VinayakUtekar <vinayakvutekar15@gmail.com>
2025-04-25 09:42:41 +0530

    Loops

---
+++ Loop/NSumFor.java
@@ -0,0 +1,16 @@
+public class NSumFor {
+    public static void main(String args[])
+    {
+        int num = Integer.parseInt(args[0]);
+        if(num > 0){
+            int  sum = 0;
+            for( int i = 1 ; i <= num ; i++ ){
+                sum = sum + i;
+            }
+            System.out.println("\n Sum IS : " + sum);
+        }
+        else{
+            System.out.println("\n INVALID INPUT ");
+        }
+    }
+}
---
+++ Loop/NintegerDoWhile.java
@@ -0,0 +1,17 @@
+class NintegerDoWhile{
+    public static void main(String args[])
+    {
+        int num = Integer.parseInt(args[0]);
+        if(num > 0){
+            int i = 1, sum = 0;
+            do{
+                sum = sum + i;
+                i = i + 1;
+            }while( i <= num);
+                System.out.println("\n Sum IS : " + sum);
+        }
+        else{
+            System.out.println("\n INVALID INPUT ");
+        }
+    }
+}
