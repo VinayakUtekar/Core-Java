2bf7770eb2d936ea3b1647f8dbee1d8aeb555556
VinayakUtekar <vinayakvutekar15@gmail.com>
2025-04-27 19:55:53 +0530

    Constructor Overloading

---
+++ OOPS/Constructor/ComputeAdd.java
@@ -0,0 +1,22 @@
+class Compute{
+
+    public static void Add(int n1,int n2){
+        int res = n1 + n2;
+        System.out.println("Result =" + res);	
+    
+    }
+    
+    public static void Add(int n1,int n2,int n3){
+        int res = n1 + n2 + n3;
+        System.out.println("Result =" + res);	
+    
+    }
+} 
+class ComputeAdd{
+    public static void main(String args[]){
+        Compute.Add(10,20);
+        Compute.Add(10,20,30);
+        Compute.Add(15,20);
+    }
+}
+    
---
+++ OOPS/Constructor/CoumputeAdd2.java
@@ -0,0 +1,28 @@
+class Compute{
+
+    public static void Add(int n1,int n2){
+        int res = n1 + n2;
+        System.out.println("Result =" + res);	
+    
+    }
+    
+    public static void Add(int n1,int n2,int n3){
+        int res = n1 + n2 + n3;
+        System.out.println("Result =" + res);	
+    }
+    
+    public static void Add(int n1,int n2,int n3,int n4){
+        int res = n1 + n2 + n3 + n4;
+        System.out.println("Result =" + res);	
+    
+    }
+    
+} 
+class ComputeAdd2{
+    public static void main(String args[]){
+        Compute.Add(10,20);
+        Compute.Add(10,20,30);
+        Compute.Add(15,20);
+        Compute.Add(10,20,30,40);
+    }
+}
