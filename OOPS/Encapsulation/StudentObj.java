d7334310d620fb331ee96a52c3e40cef9a942a0b
VinayakUtekar <vinayakvutekar15@gmail.com>
2025-04-27 19:07:41 +0530

    Encapsulation

---
+++ OOPS/Encapsulation/StudentObj.java
@@ -0,0 +1,29 @@
+class Student{
+        //variable (instence variable)(state)
+        int rno;
+        String name;
+
+        //Method
+        void setData( int r, String n){
+            rno = r;
+            name = n;
+            }
+            //Method
+            void showData(){
+            System.out.println(rno + " " + " " + name);
+        }
+}    
+public class StudentObj{
+    public static void main(String args[])
+    {
+        //object 1
+        Student s1 = new Student();
+        s1.setData(10 , "Amit");
+        s1.showData();
+
+        //object 2
+        Student s2 = new Student();
+        s2.setData(20 , "Neha");
+        s2.showData();
+    }
+}
